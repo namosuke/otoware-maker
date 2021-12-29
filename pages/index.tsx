bd63545be173962c7a93b8de686fe47c074cc9f6
ナモすけ <dynamorollerbot@gmail.com>
2021-12-29 19:07:25 +0900

    add dropzone

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,18 @@ import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/layout";
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 import styles from "./styles.module.css";
 import ReactTooltip from "react-tooltip";
 import TweetButton from "../components/tweetButton";
+import { useDropzone } from "react-dropzone";
 
 const Home: NextPage = () => {
   const onFileInputChange =
     useRef<
-      ({
-        target: { files },
-      }: React.ChangeEvent<HTMLInputElement>) => Promise<void>
+      ({ target: { files } }: React.ChangeEvent<HTMLInputElement>) => void
     >();
+  const fileConvert = useRef<(files: FileList) => void>();
   const [unsupported, setUnsupported] = useState(false);
   const [mediaSrc, setMediaSrc] = useState("");
   const [progressValue, setProgressValue] = useState(0);
@@ -26,6 +26,18 @@ const Home: NextPage = () => {
   const [outputName, setOutputName] = useState("");
   const [outputSize, setOutputSize] = useState(0);
   const inputFileRef = useRef<HTMLInputElement>(null);
+  const dropArea = useRef<HTMLDivElement>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const onDrop = useCallback((acceptedFiles) => {
+    fileConvert.current && fileConvert.current(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragAccept } = useDropzone({
+    accept: "audio/*, video/*",
+    onDrop,
+    noClick: true,
+    noKeyboard: true,
+    multiple: false,
+  });
 
   // https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
   function formatBytes(bytes: number, decimals = 2) {
@@ -54,10 +66,8 @@ const Home: NextPage = () => {
         console.error(e);
         setUnsupported(true);
       }
-      onFileInputChange.current = async ({
-        target: { files },
-      }: React.ChangeEvent<HTMLInputElement>) => {
-        if (files) {
+      fileConvert.current = async (files: FileList) => {
+        if (files.length) {
           setIsProgress(true);
           const { name } = files[0];
           const ext = name.split(".").splice(-1)[0];
@@ -121,8 +131,39 @@ const Home: NextPage = () => {
           setIsProgress(false);
         }
       };
+      onFileInputChange.current = ({
+        target: { files },
+      }: React.ChangeEvent<HTMLInputElement>) => {
+        fileConvert.current && files && fileConvert.current(files);
+      };
     })();
   }, []);
+  useEffect(() => {
+    window.addEventListener("dragenter", () => {
+      setIsDragging(true);
+    });
+    if (dropArea.current) {
+      dropArea.current.addEventListener("dragleave", () => {
+        setIsDragging(false);
+      });
+      dropArea.current.addEventListener("drop", () => {
+        setIsDragging(false);
+      });
+    }
+  }, []);
+  useEffect(() => {
+    if (dropArea.current) {
+      dropArea.current.addEventListener("dragover", (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        if (isDragging && isDragAccept && !isProgress && e.dataTransfer) {
+          e.dataTransfer.dropEffect = "copy";
+        } else if (e.dataTransfer) {
+          e.dataTransfer.dropEffect = "none";
+        }
+      });
+    }
+  }, [isDragging, isDragAccept, isProgress]);
 
   return (
     <Layout>
@@ -152,7 +193,7 @@ const Home: NextPage = () => {
           content="https://otoware-maker.vercel.app/ogp.jpg"
         />
       </Head>
-      <div className="max-w-[400px] w-[calc(100%-40px)] mx-auto pt-[20px]">
+      <div className="max-w-[400px] w-[calc(100%-100px)] mx-auto pt-[20px]">
         <Image
           src="/logo.png"
           alt="音割れメーカーのロゴ"
@@ -184,23 +225,56 @@ const Home: NextPage = () => {
             Safariなど、一部のブラウザでは使えません。別のブラウザをお試しください。
           </div>
         )}
-        <input
-          type="file"
-          accept="audio/*, video/*"
-          onChange={(e) => {
-            if (onFileInputChange.current) onFileInputChange.current(e);
-          }}
-          hidden
-          ref={inputFileRef}
-        />
-        <button
-          className="bg-gray-200 hover:bg-gray-300 border border-solid border-gray-500 rounded-[2px] px-[10px] py-[5px] block mx-auto"
-          onClick={() => {
-            inputFileRef.current?.click();
-          }}
+        {!isProgress && (
+          <>
+            <input
+              type="file"
+              accept="audio/*, video/*"
+              onChange={(e) => {
+                onFileInputChange.current && onFileInputChange.current(e);
+              }}
+              hidden
+              ref={inputFileRef}
+            />
+            <button
+              className="bg-gray-200 hover:bg-gray-300 border border-solid border-gray-500 rounded-[2px] px-[10px] py-[5px] block mx-auto"
+              onClick={() => {
+                inputFileRef.current?.click();
+              }}
+            >
+              音声・動画ファイルを選択
+            </button>
+          </>
+        )}
+        <div
+          {...getRootProps({
+            className: isDragging
+              ? isDragAccept && !isProgress
+                ? "fixed inset-0 bg-black/70 p-[10px] transition duration-200 z-10"
+                : "fixed inset-0 bg-black/70 transition duration-200 z-10"
+              : "fixed inset-0 pointer-events-none opacity-0",
+            ref: dropArea,
+            style: {},
+          })}
         >
-          音声・動画ファイルを選択
-        </button>
+          <input {...getInputProps()} />
+          {isDragging && isDragAccept && !isProgress && (
+            <div className="pointer-events-none w-[100%] h-[100%] rounded-[20px] border-[8px] border-blue-300 border-dashed grid place-items-center">
+              <p className="text-blue-300 font-bold text-center p-[10px]">
+                音声・動画ファイルをここにドロップ
+              </p>
+            </div>
+          )}
+          {(!isDragAccept || isProgress) && (
+            <div className="pointer-events-none bg-yellow-200 text-yellow-800 text-center font-bold w-[100%] p-[10px]">
+              {isProgress ? (
+                <p>変換処理中はアップロードできません。</p>
+              ) : (
+                <p>音声ファイル、動画ファイル以外はアップロードできません。</p>
+              )}
+            </div>
+          )}
+        </div>
         {isProgress && (
           <>
             <div className="text-xs overflow-hidden w-[100%] text-gray-600 text-center mt-[20px] whitespace-nowrap overflow-ellipsis">
